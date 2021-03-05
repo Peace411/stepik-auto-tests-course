c816dfc1e5b6eb851f3d835b90198eef3c2bea70
Peace411 <72517021+Peace411@users.noreply.github.com>
2021-03-06 02:11:03 +0300

    Update test_items.py

--- pytest_laung/test_items.py
+++ pytest_laung/test_items.py
@@ -5,6 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_checking_the_bucket(browser):
     browser.get(link)
-    time.sleep(5)
     assert  browser.find_element_by_css_selector("button.btn:nth-child(3)")
     
